5d9305b814b619bf3b941aa0baca58f6b8c37c5f
Dylan Corrales <deathcamel58@gmail.com>
2021-09-20 08:45:40 -0400

    Parse numbers with `+1` properly

--- util/emailParser.js
+++ util/emailParser.js
@@ -75,7 +75,10 @@ function normalizePhoneNumber(phone) {
     phone = phone.replace(/[^\d]/g, "");
 
     //check if number length equals to 10
-    if (phone.length == 10) {
+    if (phone.length === 11) {
+        phone = phone.slice(1)
+    }
+    if (phone.length === 10) {
         //reformat and return phone number
         return phone.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
     }
