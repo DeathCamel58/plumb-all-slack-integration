82287ad054fb0c30c14375dc4b3aab58d0c3643a
Dylan Corrales <deathcamel58@gmail.com>
2022-12-27 13:23:18 -0500

    Node.JS: Make changing versions easy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,19 @@
+let NODE_VERSION = "18.12.1";
+
 module.exports = {
     apps : [{
         name   : "plumb-all-slack-integration",
-        script : "npm",
+        script : `/root/.nvm/versions/node/v${NODE_VERSION}/bin/npm`,
         args : "start",
-        interpreter: "/root/.nvm/versions/node/v18.12.1/bin/node",
+        interpreter: `/root/.nvm/versions/node/v${NODE_VERSION}/bin/node`,
         env_production: {
             NODE_ENV : "production",
             ENV_LOCATION : "/root/plumb-all-slack-integration/.env",
             DEBUGGING: true
         },
+        // TODO: PM2 does not see a defined development environment
+        // > pm2 deploy development
+        // development environment is not defined in ecosystem.config.js file
         env_development: {
             NODE_ENV : "development",
             ENV_LOCATION : "/root/plumb-all-slack-integration/.env",
@@ -31,7 +36,7 @@ module.exports = {
             "ref": "origin/master",
             "repo": "https://github.com/DeathCamel58/plumb-all-slack-integration.git",
             "path": "/root/plumb-all-slack-integration",
-            "post-deploy" : "/root/.nvm/versions/node/v18.12.1/bin/npm install; pm2 startOrRestart ecosystem.config.js --env production"
+            "post-deploy" : `nvm use v${NODE_VERSION}; npm install; pm2 startOrRestart ecosystem.config.js --env production`
         }
     }
 }
