4562e41dc78e0735438e850aa0ad0e29cef6832d
Dylan Corrales <deathcamel58@gmail.com>
2022-12-30 07:54:36 -0500

    APICoordinator: Add to make sending to all necessary APIs easy

--- util/emailParser.js
+++ util/emailParser.js
@@ -1,6 +1,8 @@
 let Contact = require('./contact.js');
 let PostHog = require('./apis/PostHog.js');
 let Trello = require("./apis/Trello");
+let APICoordinator = require("./APICoordinator");
+
 module.exports = {
     parseMessageFromAnswerphone,
     parseMessageFromWebsite,
@@ -33,8 +35,7 @@ function parseMessageFromAnswerphone(message) {
     }
 
     let contact = new Contact("Call", name, phone, callerid, undefined, fullAddress, contactMessage);
-    PostHog.logContact(contact, message);
-    Trello.addContact(contact);
+    APICoordinator.contactMade(contact, message);
     return contact;
 }
 
@@ -55,8 +56,7 @@ function parseMessageFromWebsite(message) {
     let contactMessage = cleanText(parts[5].split("message:")[1]);
 
     let contact = new Contact("Message From Website", name, phone, undefined, email, address, contactMessage);
-    PostHog.logContact(contact, message);
-    Trello.addContact(contact);
+    APICoordinator.contactMade(contact, message);
     return contact;
 }
 
@@ -77,8 +77,7 @@ function parseMessageFromJobber(message) {
     contactMessage = contactMessage + "|Details in Jobber> (You may have to hold on that link, copy it, and paste it into your web browser to access it)";
 
     let contact = new Contact("Message From Jobber Request", name, phone, undefined, email, address, contactMessage);
-    PostHog.logContact(contact, message);
-    Trello.addContact(contact);
+    APICoordinator.contactMade(contact, message);
     return contact;
 }
 
@@ -118,8 +117,7 @@ function parseMessageFromGoogleAds(message) {
     }
 
     let contact = new Contact("Lead from Google Ads", details['name'], details['phone'], undefined, details['email'], details['city'], details['message']);
-    PostHog.logContact(contact, message);
-    Trello.addContact(contact);
+    APICoordinator.contactMade(contact, message);
     return contact;
 }
 
