637df7a4c6846f9dfc4b0873e702c995a5f128d6
Dylan Corrales <deathcamel58@gmail.com>
2023-02-20 11:12:42 -0500

    PM2: Deployment fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,19 +3,19 @@ let NODE_VERSION = "18.14.1";
 module.exports = {
     apps : [{
         name   : "plumb-all-slack-integration",
-        script : `/home/ubuntu/.nvm/versions/node/v${NODE_VERSION}/bin/npm`,
+        script : `$HOME/.nvm/versions/node/v${NODE_VERSION}/bin/npm`,
         args : "start",
-        interpreter: `/home/ubuntu/versions/node/v${NODE_VERSION}/bin/node`,
+        interpreter: `$HOME/versions/node/v${NODE_VERSION}/bin/node`,
         env_production: {
             NODE_ENV : "production",
-            ENV_LOCATION : "/home/ubuntu/plumb-all-slack-integration/.env"
+            ENV_LOCATION : "$HOME/plumb-all-slack-integration/.env"
         },
         // TODO: PM2 does not see a defined development environment
         // > pm2 deploy development
         // development environment is not defined in ecosystem.config.js file
         env_development: {
             NODE_ENV : "development",
-            ENV_LOCATION : "/home/ubuntu/plumb-all-slack-integration/.env",
+            ENV_LOCATION : "$HOME/plumb-all-slack-integration/.env",
             DEBUGGING: true
         },
         autorestart: true,
@@ -23,7 +23,7 @@ module.exports = {
         ignore_path: [
             "node_modules"
         ],
-        log_file: '/home/ubuntu/plumb-all-slack-integration/runtime.log'
+        log_file: '$HOME/plumb-all-slack-integration/runtime.log'
     }],
 
     // Deployment to server configuration
@@ -35,7 +35,7 @@ module.exports = {
             "ref": "origin/master",
             "repo": "https://github.com/DeathCamel58/plumb-all-slack-integration.git",
             "path": "/home/ubuntu/plumb-all-slack-integration",
-            "post-deploy" : `nvm use v${NODE_VERSION}; npm install; pm2 startOrRestart ecosystem.config.js --env production`
+            "post-deploy" : `source $HOME/.nvm/nvm.sh; nvm use v${NODE_VERSION}; npm install; pm2 startOrRestart ecosystem.config.js --env production`
         }
     }
 }
