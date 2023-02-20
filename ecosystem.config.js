1da921635f7dfe8f98b3cbb4233f1d5bef27b3bb
Dylan Corrales <deathcamel58@gmail.com>
2023-02-20 11:04:18 -0500

    Move deployment to EC2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,21 @@
-let NODE_VERSION = "18.12.1";
+let NODE_VERSION = "18.14.1";
 
 module.exports = {
     apps : [{
         name   : "plumb-all-slack-integration",
-        script : `/root/.nvm/versions/node/v${NODE_VERSION}/bin/npm`,
+        script : `/home/ubuntu/.nvm/versions/node/v${NODE_VERSION}/bin/npm`,
         args : "start",
-        interpreter: `/root/.nvm/versions/node/v${NODE_VERSION}/bin/node`,
+        interpreter: `/home/ubuntu/versions/node/v${NODE_VERSION}/bin/node`,
         env_production: {
             NODE_ENV : "production",
-            ENV_LOCATION : "/root/plumb-all-slack-integration/.env"
+            ENV_LOCATION : "/home/ubuntu/plumb-all-slack-integration/.env"
         },
         // TODO: PM2 does not see a defined development environment
         // > pm2 deploy development
         // development environment is not defined in ecosystem.config.js file
         env_development: {
             NODE_ENV : "development",
-            ENV_LOCATION : "/root/plumb-all-slack-integration/.env",
+            ENV_LOCATION : "/home/ubuntu/plumb-all-slack-integration/.env",
             DEBUGGING: true
         },
         autorestart: true,
@@ -23,18 +23,18 @@ module.exports = {
         ignore_path: [
             "node_modules"
         ],
-        log_file: '/root/plumb-all-slack-integration/runtime.log'
+        log_file: '/home/ubuntu/plumb-all-slack-integration/runtime.log'
     }],
 
     // Deployment to server configuration
     deploy: {
         production: {
             "key": "deploy.pem",
-            "user": "root",
+            "user": "ubuntu",
             "host": "pm2.plumb-all.com",
             "ref": "origin/master",
             "repo": "https://github.com/DeathCamel58/plumb-all-slack-integration.git",
-            "path": "/root/plumb-all-slack-integration",
+            "path": "/home/ubuntu/plumb-all-slack-integration",
             "post-deploy" : `nvm use v${NODE_VERSION}; npm install; pm2 startOrRestart ecosystem.config.js --env production`
         }
     }
