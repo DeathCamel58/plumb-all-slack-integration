027cefd61a6cf2d3475b64afab3c5c451fd8bdaf
Dylan Corrales <deathcamel58@gmail.com>
2024-01-25 09:15:15 -0500

    Hotfix: Event listener registration

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 require('./util/web');
+
+// Require files to allow them to register events
+const files = [
+    './util/apis/CloudFlareWorkers',
+    './util/apis/FleetSharp',
+    './util/apis/GoogleAds',
+    './util/apis/JobberWebHookHandler',
+    './util/apis/SasoWebHookHandler'
+];
+for (let i = 0; i < files.length; i++) {
+    let tmp = require(files[i]);
+}
