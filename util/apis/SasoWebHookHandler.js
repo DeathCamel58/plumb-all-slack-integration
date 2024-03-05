32f58812111366682de430b856928a9f86d37398
Dylan Corrales <deathcamel58@gmail.com>
2024-03-05 08:28:30 -0500

    SASO: Change to JSON
    They finally fixed their special character escaping.

--- util/apis/SasoWebHookHandler.js
+++ util/apis/SasoWebHookHandler.js
@@ -2,38 +2,17 @@ const Contact = require("../contact");
 const APICoordinator = require("../APICoordinator");
 const events = require('../events');
 
-function parseBody(body) {
-    let parsed = body.split("039f7a43-a61a-4057-955b-e0d0818d3437");
-
-    for (let i = 0; i < parsed.length; i++) {
-        parsed[i] = parsed[i].replace(/^\s+|\s+$/g, '');
-    }
-
-    let data = {}
-
-    data.first_name = parsed[0];
-    data.last_name = parsed[1];
-    data.phone_number = parsed[2];
-    data.called_from = parsed[3];
-    data.address = parsed[4];
-    data.message = parsed[5];
-    data.call_source = parsed[6];
-    data.agent_notes = parsed[7];
-
-    return data;
-}
-
 /**
  * Takes in a lead from SASO,
  * @param req The incoming web data
  * @returns {Promise<void>}
  */
 async function leadHandle(req) {
-    let body = parseBody(req.body);
+    let body = JSON.parse(req.body);
 
     let message = "";
-    if (body.message) {
-        message += body.message;
+    if (body.ticket_message) {
+        message += body.ticket_message;
     }
     if (body.agent_notes) {
         if (message !== "") {
@@ -42,7 +21,7 @@ async function leadHandle(req) {
         }
     }
 
-    let contact = new Contact("Call", `${body.first_name} ${body.last_name}`, body.phone_number, body.called_from, undefined, body.address, message, body.call_source);
+    let contact = new Contact("Call", `${body.first_name} ${body.last_name}`, body.main_phone_number, body.caller_id, undefined, body.address, message, body.call_source);
 
     // Send the request to where it needs to go
     await APICoordinator.contactMade(contact, JSON.stringify(body));
