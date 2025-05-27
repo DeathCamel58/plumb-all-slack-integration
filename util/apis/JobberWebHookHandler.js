8a882465fc2e5b5da1bbf99925f88c60d3136dd6
Dylan Corrales <deathcamel58@gmail.com>
2025-05-27 09:04:14 -0400

    Jobber Request: Fix bad link sent to Slack/Mattermost Fixes: #11

--- util/apis/JobberWebHookHandler.js
+++ util/apis/JobberWebHookHandler.js
@@ -377,7 +377,7 @@ async function requestCreateHandle(req) {
       null,
       request.client.emails[0].address,
       address,
-      `[${request.jobberWebUri}](Details in Jobber) (You may have to hold on that link, copy it, and paste it into your web browser to access it)`,
+      `[${request.jobberWebUri}](${request.jobberWebUri}) (You may have to hold on that link, copy it, and paste it into your web browser to access it)`,
       "Jobber",
     );
     await APICoordinator.contactMade(contact, JSON.stringify(body));
